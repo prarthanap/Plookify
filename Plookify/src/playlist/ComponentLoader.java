9283d53a515f36dfdc0cb610ec3001734085f5b2
ec14092 <>
2016-01-19 18:22:38 +0000

    Adding playlist package

---
+++ Plookify/src/playlist/ComponentLoader.java
@@ -0,0 +1,14 @@
+package playlist;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Edgar
+ */
+public class ComponentLoader {
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "The application works.");
+    }
+}
