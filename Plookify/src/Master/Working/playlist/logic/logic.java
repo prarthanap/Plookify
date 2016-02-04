610193a3f616914e43ed48aafd0f30d8d905444f
ec14092 <>
2016-02-04 23:46:03 +0000

    Editing playlist package

---
+++ Plookify/src/Master/Working/playlist/logic/logic.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Master.Working.playlist.logic;
+
+/**
+ *
+ * @author Edgar
+ */
+public class logic {
+    
+    public void createPlaylist()
+    {
+    
+    }
+    
+    public void renamePlaylist()
+    {
+        
+    }
+    
+    public void sortPlaylist()
+    {
+        
+    }
+    
+    public void addToPlaylist()
+    {
+        
+    }
+}
