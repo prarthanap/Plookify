7425675214c68361cf196e9096463f8d8f1d44c5
Edgaras Rinkevicius <e.rinkevicius@se14.qmul.ac.uk>
2016-02-13 20:00:40 +0000

    basic GUI code implementation

--- Plookify/src/Master/Working/playlist/gui/gui.java
+++ Plookify/src/Master/Working/playlist/gui/gui.java
@@ -1,15 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Master.Working.playlist.gui;
 
+package Master.Working.playlist.gui;
 /**
  *
  * @author Edgar
  */
-public class gui {
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class gui extends Application implements EventHandler<ActionEvent>{
     
-    //
+    Button newPlaylist;
+    
+    public static void main(String[] args){
+    launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        primaryStage.setTitle("Playlist");
+        newPlaylist = new Button();
+        newPlaylist.setText("New Playlist");
+        
+        newPlaylist.setOnAction(this);
+    
+        StackPane layout = new StackPane();
+        layout.getChildren().add(newPlaylist);
+        
+        Scene scene = new Scene(layout,1100,600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+       
+    }
+
+    @Override
+    public void handle(ActionEvent t) {
+        //
+    }
+    
+    
 }
