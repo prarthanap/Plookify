fc0532199793fe2e304d397bfcc442d3158047ef
h.qureashi <h.qureashi@se14.qmul.ac.uk>
2016-02-18 13:49:40 +0000

    Creating main GUI

--- Plookify/src/Master/Working/social/ComponentLoader.java
+++ Plookify/src/Master/Working/social/ComponentLoader.java
@@ -1,10 +1,14 @@
 /* @author Hamza */
 package Master.Working.social;
+import Master.Working.social.Premium.Premium;
 import javax.swing.JOptionPane;
 public class ComponentLoader
 {    
     public static void main(String[] args)
     {
+      Premium premium = new Premium();
+      
+      
       JOptionPane.showMessageDialog(null, "The application works");
     }
 }
--- Plookify/src/Master/Working/social/Premium/Premium.java
+++ Plookify/src/Master/Working/social/Premium/Premium.java
@@ -7,6 +7,9 @@ import java.util.*;
  */
 public class Premium extends account{
     
+    public Premium()
+    {}
+        
     public void accType()
     {}
     
