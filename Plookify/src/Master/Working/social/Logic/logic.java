6eb34634e596433d6ab2624fa735afdbc255c631
h.qureashi <h.qureashi@se14.qmul.ac.uk>
2016-02-24 13:15:25 +0000

    Implementing buttons

--- Plookify/src/Master/Working/social/Logic/logic.java
+++ Plookify/src/Master/Working/social/Logic/logic.java
@@ -5,39 +5,78 @@
  */
 package Master.Working.social.Logic;
 
-/**
- *
- * @author Hamza
- */
+/**** @author Hamza */
+
+import Master.Working.Common.database;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class logic {
     
-    public void accTypeCheck()
+    private database data=new database();
+    
+    public logic()
     {
-        
+    
     }
     
-    public void authCheck()
+    public boolean accTypeCheck(String premium, String uname)
     {
-        
+        String id = "SELECT ID FROM ACCOUNT WHERE USERNAME='"+uname+"'";
+        String paid = "SELECT USERID AND SUBSCRIPTION TYPE FROM SUBCRIPTION WHERE USERID='"+id+"'";
+        if(paid.equals("PREMIUM"))
+        {
+            return true;       
+        }
+        return false;
     }
     
-    public void idPull()
+    public int authCheck(String uname, String pass1)
     {
-        
+        String authtest="SELECT ID FROM ACCOUNT WHERE USERNAME='"+uname+"' and PASSWORD='"+pass1+"'";
+        if(data.dupcheck(authtest))
+        {
+            try {
+                authtest="SELECT ID FROM ACCOUNT WHERE USERNAME='"+uname+"' and PASSWORD='"+pass1+"'";
+                return data.makeQuery(authtest).getInt(1);
+                }
+            catch (SQLException ex) {
+                Logger.getLogger(Master.Working.account.logic.logic.class.getName()).log(Level.SEVERE, null, ex);
+            }
+}
+        return 9999;
     }
-    
-        public void accType()
-    {}
     
     public void findFriend()
-    {}
-    
-    public void discover()
-    {}
-    
-    public boolean friendRequest()
     {
-     return false;
+    
+    
+    }
+    
+    public void discover(String uname, String premium)
+    {
+        boolean check = accTypeCheck(uname, premium);
+        if(check==true)
+        {
+        
+        
+        }
+        
+    }
+    
+    public void friendRequest(String uname, String friendname)//checks if username exists then inserts record
+    {
+        if(data.dupcheck(uname,"USERNAME","ACCOUNT"))
+        {System.out.println(true);}
+        else
+        {
+            System.out.println(false);
+            String update="INSERT INTO FRIENDLIST (OWNERID,FRIENDID) VALUES('"+uname+"','"+friendname+"')";
+            //System.out.println(update);
+            data.makeUpdate(update);
+            System.out.println("added");
+        }
     }
     
     public int friendList()
@@ -48,5 +87,8 @@ public class logic {
     }
     
     public void friendPlaylist()
-    {}
+    {
+    
+    
+    }
 }
