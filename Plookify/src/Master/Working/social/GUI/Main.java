f45c56f596361d6ebb7f8da3aeef154657ee2bba
h.qureashi <h.qureashi@se14.qmul.ac.uk>
2016-02-28 15:00:19 +0000

    Implementing logic and organising files

---
+++ Plookify/src/Master/Working/social/GUI/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Master.Working.social.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+//        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+//        Scene scene = new Scene(root);
+//        stage.setScene(scene);
+////        stage.setTitle("Plookify");
+//        stage.setResizable(false);
+//        stage.show();
+
+        
+        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+        primaryStage.setTitle("Plookify");
+        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.show();
+    }
+}
