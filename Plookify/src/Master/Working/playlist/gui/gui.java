5cf87526805d6a041c6d871dd3afe2e5387d7da9
Edgaras Rinkevicius <e.rinkevicius@se14.qmul.ac.uk>
2016-03-01 23:29:20 +0000

    GUI changes and controller

--- Plookify/src/Master/Working/playlist/gui/gui.java
+++ Plookify/src/Master/Working/playlist/gui/gui.java
@@ -5,18 +5,13 @@ package Master.Working.playlist.gui;
  * @author Edgar
  */
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.event.ActionEvent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
 
-public class gui extends Application implements EventHandler<ActionEvent>{
+public class gui extends Application{
     
-    Button newPlaylist;
     
     public static void main(String[] args){
     launch(args);
@@ -25,16 +20,9 @@ public class gui extends Application implements EventHandler<ActionEvent>{
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("playlist.fxml"));
-        Scene scene = new Scene(root,1100,600);
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
        
-    }
-
-    @Override
-    public void handle(ActionEvent t) {
-        
-    }
-    
-    
+    } 
 }
