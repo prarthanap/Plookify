f2a71889fdc429e0615bf204b2a1775c54009584
ec14002 <j.l.leow@se14.qmul.ac.uk>
2016-03-20 21:53:11 +0000

    code for hamza in test2

--- Plookify/src/Master/Working/Common/test2.java
+++ Plookify/src/Master/Working/Common/test2.java
@@ -5,14 +5,33 @@
  */
 package Master.Working.Common;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author jll30
  */
 public class test2
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws SQLException
+    {//remember to use try catch block
+        database data=new database();
+        ResultSet pubID=data.makeQuery("SELECT USERID FROM SUBSCRIPTION WHERE PREMIUM=1 and PUBLICITY='0.0'");
+        ArrayList<Integer> pID=new ArrayList<>();
+        while (pubID.next())
+        {
+            pID.add(pubID.getInt(1));
+        }
+        ArrayList<String> namesList=new ArrayList<>();
+        for(int i=0;i<pID.size();i++)
+        {
+            namesList.add(data.makeQuery("SELECT USERNAME FROM ACCOUNT WHERE ID='"+pID.get(i)+"'").getString(1));
+        }
+        for (String a : namesList)
+        {System.out.println(a);}
+        
         
     }
             
